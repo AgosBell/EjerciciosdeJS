766bb6c87d82f68ef56a4b96ac632f9c77b8f511
Agostina Bellotti <agostinabell@gmail.com>
2023-10-26 13:18:37 -0300

    Agregué ejercicio número 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,5 @@
+/*Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.*/
+
+let numero1= prompt("Ingrese un primer número")
+let numero2= prompt("Ingrese un segundo número")
+console.log ("el numero "+numero1+" es mayor que el numero "+numero2);
