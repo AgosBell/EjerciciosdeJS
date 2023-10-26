433722c3cebad631348ab0bcb6589f02371ac06d
Agostina Bellotti <agostinabell@gmail.com>
2023-10-26 13:48:07 -0300

    Agregué ejercicio 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,6 @@
+//Escribe un programa que pida un número y diga si es divisible por 2//
+
+let numeroSeleccionado=prompt("Ingrese un número")
+if (numeroSeleccionado / 2){
+    console.log("El numero "+numeroSeleccionado+ " es divisible por 2")
+}
